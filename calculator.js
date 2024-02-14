d05bc4b239c17716da431ec50fbf5ff1e1081bf4
PramakantKodamsingh <140236716+PramakantKodamsingh@users.noreply.github.com>
2024-02-15 00:33:27 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,11 +1,7 @@
 
 var inp=document.getElementById("ser");
 var btn=document.getElementsByTagName("button");
-
 let string=""
-// let arr=Array.from(btn);
-// arr.forEach(btn=>{
-//   btn.addEventListener('click',function(e){
 for(let i=0;i<=19;i++){
       btn[i].addEventListener('click',function(e){
         if(e.target.innerHTML == '='){
@@ -27,5 +23,4 @@ for(let i=0;i<=19;i++){
            
     });
 }
-// btn[0].addEventListener('click',get);
-// btn[0].addEventListener('mouseleave',geti)
+
